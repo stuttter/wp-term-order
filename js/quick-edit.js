bc4781b573a9f11ddf3482d298486f95c3b2d515
John James Jacoby <johnjamesjacoby@me.com>
2016-01-08 23:49:31 -0600

    Simplify jQuery usage.

--- js/quick-edit.js
+++ js/quick-edit.js
@@ -1,8 +1,8 @@
-jQuery( document ).ready( function() {
-    jQuery( '.editinline' ).on( 'click', function() {
-        var tag_id = jQuery( this ).parents( 'tr' ).attr( 'id' ),
-			order  = jQuery( 'td.order', '#' + tag_id ).text();
+jQuery( document ).ready( function( $ ) {
+    $( '.editinline' ).on( 'click', function() {
+        var tag_id = $( this ).parents( 'tr' ).attr( 'id' ),
+			order  = $( 'td.order', '#' + tag_id ).text();
 
-        jQuery( ':input[name="order"]', '.inline-edit-row' ).val( order );
+        $( ':input[name="order"]', '.inline-edit-row' ).val( order );
     } );
 } );
